905fc0c4f64bee65d062766203795dfb7b64cb51
Mark-Haze <markhazen05@hotmail.com>
2024-03-01 18:12:15 -0600

    Subkey Generation Complete

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -9,25 +9,173 @@ module GenerateKeys (Key, SubKey1, SubKey2, SubKey3, SubKey4,
 		     SubKey9, SubKey10, SubKey11, SubKey12,
 		     SubKey13, SubKey14, SubKey15, SubKey16);
    
-   // Generate SubKeys
-   input logic [63:0]  Key;
-   output logic [47:0] SubKey1;
-   output logic [47:0] SubKey2;
-   output logic [47:0] SubKey3;
-   output logic [47:0] SubKey4;
-   output logic [47:0] SubKey5;
-   output logic [47:0] SubKey6;
-   output logic [47:0] SubKey7;
-   output logic [47:0] SubKey8;
-   output logic [47:0] SubKey9;
-   output logic [47:0] SubKey10;
-   output logic [47:0] SubKey11;
-   output logic [47:0] SubKey12;
-   output logic [47:0] SubKey13;
-   output logic [47:0] SubKey14;
-   output logic [47:0] SubKey15;
-   output logic [47:0] SubKey16;
+	// Generate SubKeys
+	input logic [63:0]  Key;
+	output logic [47:0] SubKey1;
+	output logic [47:0] SubKey2;
+	output logic [47:0] SubKey3;
+	output logic [47:0] SubKey4;
+	output logic [47:0] SubKey5;
+	output logic [47:0] SubKey6;
+	output logic [47:0] SubKey7;
+	output logic [47:0] SubKey8;
+	output logic [47:0] SubKey9;
+	output logic [47:0] SubKey10;
+	output logic [47:0] SubKey11;
+	output logic [47:0] SubKey12;
+	output logic [47:0] SubKey13;
+	output logic [47:0] SubKey14;
+	output logic [47:0] SubKey15;
+	output logic [47:0] SubKey16;
 
+	logic [27:0] leftPerm;
+	logic [27:0] rightPerm;
+
+	PC1 pc1(Key, leftPerm, rightPerm);
+
+	//Shift 1
+	logic [27:0] leftShift1;
+	logic [27:0] rightShift1;
+
+	assign leftShift1 = {leftPerm[26:0], leftPerm[27]};
+	assign rightShift1 = {rightPerm[26:0], rightPerm[27]};
+
+	PC2 key1Out(leftShift1, rightShift1, SubKey1);
+
+	//Shift 2
+	logic [27:0] leftShift2;
+	logic [27:0] rightShift2;
+	
+	assign  leftShift2 = {leftShift1[26:0], leftShift1[27]};
+	assign  rightShift2 = {rightShift1[26:0], rightShift1[27]};
+
+	PC2 key2Out(leftShift2, rightShift2, SubKey2);
+
+	//Shift 3
+	logic [27:0] leftShift3;
+	logic [27:0] rightShift3;
+	
+	assign  leftShift3 = {leftShift2[25:0], leftShift2[27:26]};
+	assign  rightShift3 = {rightShift2[25:0], rightShift2[27:26]};
+
+	PC2 key3Out(leftShift3, rightShift3, SubKey3);
+
+	//Shift 4
+	logic [27:0] leftShift4;
+	logic [27:0] rightShift4;
+
+	assign  leftShift4 = {leftShift3[25:0], leftShift3[27:26]};
+	assign  rightShift4 = {rightShift3[25:0], rightShift3[27:26]};
+
+	PC2 key4Out(leftShift4, rightShift4, SubKey4);
+
+	//Shift 5
+	logic [27:0] leftShift5;
+	logic [27:0] rightShift5;
+
+	assign  leftShift5 = {leftShift4[25:0], leftPerm[27:26]};
+	assign  rightShift5 = {leftShift4[25:0], rightPerm[27:26]};
+
+	PC2 key5Out(leftShift5, rightShift5, SubKey5);
+
+	//Shift 6
+	logic [27:0] leftShift6;
+	logic [27:0] rightShift6;
+
+	assign leftShift6 = {leftShift5[25:0], leftShift5[27:26]};
+	assign rightShift6 = {rightShift5[25:0], rightShift5[27:26]};
+
+	PC2 key6Out(leftShift6, rightShift6, SubKey6);
+
+	//Shift 7
+	logic [27:0] leftShift7;
+	logic [27:0] rightShift7;
+
+	assign leftShift7 = {leftShift6[25:0], leftShift6[27:26]};
+	assign rightShift7 = {rightShift6[25:0], rightShift6[27:26]};
+
+	PC2 key7Out(leftShift7, rightShift7, SubKey7);
+
+	//Shift 8
+	logic [27:0] leftShift8;
+	logic [27:0] rightShift8;
+
+	assign leftShift8 = {leftShift7[25:0], leftShift7[27:26]};
+	assign rightShift8 = {rightShift7[25:0], rightShift7[27:26]};
+
+	PC2 key8Out(leftShift8, rightShift8, SubKey8);
+
+	//Shift 9
+	logic [27:0] leftShift9;
+	logic [27:0] rightShift9;
+
+	assign leftShift9 = {leftShift8[26:0], leftShift8[27]};
+	assign rightShift9 = {rightShift8[26:0], rightShift8[27]};
+
+	PC2 key9Out(leftShift9, rightShift9, SubKey9);
+
+	//Shift 10
+	logic [27:0] leftShift10;
+	logic [27:0] rightShift10;
+	
+	assign leftShift10 = {leftShift9[25:0], leftShift9[27:26]};
+	assign rightShift10 = {rightShift9[25:0], rightShift9[27:26]};
+
+	PC2 key10Out(leftShift10, rightShift10, SubKey10);
+
+	//Shift 11
+	logic [27:0] leftShift11;
+	logic [27:0] rightShift11;
+	
+	assign leftShift11 = {leftShift10[25:0], leftShift10[27:26]};
+	assign rightShift11 = {rightShift10[25:0], rightShift10[27:26]};
+
+	PC2 key11Out(leftShift11, rightShift11, SubKey11);
+
+	//Shift 12
+	logic [27:0] leftShift12;
+	logic [27:0] rightShift12;
+	
+	assign leftShift12 = {leftShift11[25:0], leftShift11[27:26]};
+	assign rightShift12 = {rightShift11[25:0], rightShift11[27:26]};
+
+	PC2 key12Out(leftShift12, rightShift12, SubKey12);
+
+	//Shift 13
+	logic [27:0] leftShift13;
+	logic [27:0] rightShift13;
+
+	assign leftShift13 = {leftShift12[25:0], leftShift12[27:26]};
+	assign rightShift13 = {rightShift12[25:0], rightShift12[27:26]};
+
+	PC2 key13Out(leftShift13, rightShift13, SubKey13);
+
+	//Shift 14
+	logic [27:0] leftShift14;
+	logic [27:0] rightShift14;
+	
+	assign leftShift14 = {leftShift13[25:0], leftShift13[27:26]};
+	assign rightShift14 = {rightShift13[25:0], rightShift13[27:26]};
+
+	PC2 key14Out(leftShift14, rightShift14, SubKey14);
+
+	//Shift 15
+	logic [27:0] leftShift15;
+	logic [27:0] rightShift15;
+
+	assign leftShift15 = {leftShift14[25:0], leftShift14[27:26]};
+	assign rightShift15 = {rightShift14[25:0], rightShift14[27:26]};
+
+	PC2 key15Out(leftShift15, rightShift15, SubKey15);
+
+	//Shift 16
+	logic [27:0] leftShift16;
+	logic [27:0] rightShift16;
+
+	assign leftShift16 = {leftShift15[26:0], leftShift15[27]};
+	assign rightShift16 = {rightShift15[26:0], rightShift15[27]};
+
+	PC2 key16Out(leftShift16, rightShift16, SubKey16);
 endmodule // GenerateKeys
 
 module PC1 (key, left_block, right_block);
@@ -36,13 +184,73 @@ module PC1 (key, left_block, right_block);
    	output logic [27:0] left_block;
    	output logic [27:0] right_block;
 
-	logic PCKey
-
-   	IP ip(key, PCKey);
-
-   	logic [55:0]        out_block;
+	PC1Table pc1Table(key, left_block, right_block);
 
 endmodule // PC1
+
+module PC1Table (key, left, right);
+	input logic [63:0] key;
+	output logic [27:0] left;
+	output logic [27:0] right;
+
+	assign left[27] = key[64-57];
+	assign left[26] = key[64-49];
+	assign left[25] = key[64-41];
+	assign left[24] = key[64-33];
+	assign left[23] = key[64-25];
+	assign left[22] = key[64-17];
+	assign left[21] = key[64-9];
+	assign left[20] = key[64-1];
+	assign left[19] = key[64-58];
+	assign left[18] = key[64-50];
+	assign left[17] = key[64-42];
+	assign left[16] = key[64-34];
+	assign left[15] = key[64-26];
+	assign left[14] = key[64-18];
+	assign left[13] = key[64-10];
+	assign left[12] = key[64-2];
+	assign left[11] = key[64-59];
+	assign left[10] = key[64-51];
+	assign left[9] = key[64-43];
+	assign left[8] = key[64-35];
+	assign left[7] = key[64-27];
+	assign left[6] = key[64-19];
+	assign left[5] = key[64-11];
+	assign left[4] = key[64-3];
+	assign left[3] = key[64-60];
+	assign left[2] = key[64-52];
+	assign left[1] = key[64-44];
+	assign left[0] = key[64-36];
+	
+	assign right[27] = key[64-63];
+	assign right[26] = key[64-55];
+	assign right[25] = key[64-47];
+	assign right[24] = key[64-39];
+	assign right[23] = key[64-31];
+	assign right[22] = key[64-23];
+	assign right[21] = key[64-15];
+	assign right[20] = key[64-7];
+	assign right[19] = key[64-62];
+	assign right[18] = key[64-54];
+	assign right[17] = key[64-46];
+	assign right[16] = key[64-38];
+	assign right[15] = key[64-30];
+	assign right[14] = key[64-22];
+	assign right[13] = key[64-14];
+	assign right[12] = key[64-6];
+	assign right[11] = key[64-61];
+	assign right[10] = key[64-53];
+	assign right[9] = key[64-45];
+	assign right[8] = key[64-37];
+	assign right[7] = key[64-29];
+	assign right[6] = key[64-21];
+	assign right[5] = key[64-13];
+	assign right[4] = key[64-5];
+	assign right[3] = key[64-28];
+	assign right[2] = key[64-20];
+	assign right[1] = key[64-12];
+	assign right[0] = key[64-4];
+endmodule
 
 module PC2 (left_block, right_block, subkey);
 
@@ -50,7 +258,69 @@ module PC2 (left_block, right_block, subkey);
    input logic [27:0] right_block;
    output logic [47:0] subkey;
 
+   PC2Table pc2Table(left_block, right_block, subkey);
+
 endmodule // PC2
+
+module PC2Table(left, right, subkey);
+	input logic [27:0] left;
+	input logic [27:0] right;
+	output logic [47:0] subkey;
+
+	logic [55:0] combined;
+
+	assign combined[55:28] = left;
+	assign combined[27:0] = right;
+
+	assign subkey[47] = combined[56-14];
+	assign subkey[46] = combined[56-17];
+	assign subkey[45] = combined[56-11];
+	assign subkey[44] = combined[56-24];
+	assign subkey[43] = combined[56-1];
+	assign subkey[42] = combined[56-5];
+	assign subkey[41] = combined[56-3];
+	assign subkey[40] = combined[56-28];
+	assign subkey[39] = combined[56-15];
+	assign subkey[38] = combined[56-6];
+	assign subkey[37] = combined[56-21];
+	assign subkey[36] = combined[56-10];
+	assign subkey[35] = combined[56-23];
+	assign subkey[34] = combined[56-19];
+	assign subkey[33] = combined[56-12];
+	assign subkey[32] = combined[56-4];
+	assign subkey[31] = combined[56-26];
+	assign subkey[30] = combined[56-8];
+	assign subkey[29] = combined[56-16];
+	assign subkey[28] = combined[56-7];
+	assign subkey[27] = combined[56-27];
+	assign subkey[26] = combined[56-20];
+	assign subkey[25] = combined[56-13];
+	assign subkey[24] = combined[56-2];
+	assign subkey[23] = combined[56-41];
+	assign subkey[22] = combined[56-52];
+	assign subkey[21] = combined[56-31];
+	assign subkey[20] = combined[56-37];
+	assign subkey[19] = combined[56-47];
+	assign subkey[18] = combined[56-55];
+	assign subkey[17] = combined[56-30];
+	assign subkey[16] = combined[56-40];
+	assign subkey[15] = combined[56-51];
+	assign subkey[14] = combined[56-45];
+	assign subkey[13] = combined[56-33];
+	assign subkey[12] = combined[56-48];
+	assign subkey[11] = combined[56-44];
+	assign subkey[10] = combined[56-49];
+	assign subkey[9] = combined[56-39];
+	assign subkey[8] = combined[56-56];
+	assign subkey[7] = combined[56-34];
+	assign subkey[6] = combined[56-53];
+	assign subkey[5] = combined[56-46];
+	assign subkey[4] = combined[56-42];
+	assign subkey[3] = combined[56-50];
+	assign subkey[2] = combined[56-36];
+	assign subkey[1] = combined[56-29];
+	assign subkey[0] = combined[56-32];
+endmodule
 
 // Straight Function
 module SF (inp_block, out_block);
